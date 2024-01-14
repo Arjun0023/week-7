6f0daa3d389c31fb1b64139c018bb305435da465
Arjun Pawar <108388620+Arjun0023@users.noreply.github.com>
2024-01-14 20:21:42 +0530

    code

--- src/store/atoms/count.jsx
+++ src/store/atoms/count.jsx
@@ -1,6 +1,15 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export const countAtom = atom({
     key: "countAtom",
     default: 0
+});
+
+
+export const evenSelector = selector({
+    name:"evenSelector",
+    get : (props) =>{
+        const count = props.get(countAtom);
+        return count%2;
+    }
 });
